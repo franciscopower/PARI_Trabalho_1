5c7a26e112bca504c86a53bfadfa356a877b4627
Francisco Power <franciscopower66@gmai.com>
2020-10-21 15:42:16 +0100

    main organization

--- main.py
+++ main.py
@@ -1,9 +1,40 @@
+# Imports
+from collections import namedtuple
+from time import time
+
+
+def test_line():
+    pass
+
+
+
+def statistics(inputs):
+    """
+    Create Statistics dictionary
+    :param value: list of named tuples with test results
+    :return: dictionary of statistics
+    """
+    pass
+
+
+
 def main():
     """
     Typing test
     """
     
-    pass
+    # Parsing. 
+    # Create 2 parsing arguments, --use_time_code (no input) and --max_value (Max Value)
+    
+    cond = False #substitute with condition according to the parsed inputs
+    
+    while cond:
+        test_line()
+        
+
+
+
+
 
 if __name__== "__main__":
     main()
