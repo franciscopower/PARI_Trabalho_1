95781b9430591f4963f4f897e1324c173d17b9bb
Francisco Power <franciscopower66@gmai.com>
2020-10-21 16:18:06 +0100

    parsing arguments

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python
+
 # Imports
 from collections import namedtuple
 from time import time
+import argparse
 
 
 def test_line():
@@ -22,9 +25,16 @@ def main():
     """
     Typing test
     """
-    
+
     # Parsing. 
-    # Create 2 parsing arguments, --use_time_code (no input) and --max_value (Max Value)
+    parser = argparse.ArgumentParser(description = "Definition of test mode")
+    parser.add_argument('-utm', '--user_time_mode', action='store_true', help = 'If used, test will be time based.')
+    parser.add_argument('-mv', '--max_value', help = 'Max number of secs for time mode or maximum number of inputs for number of inputs mode.', required=True)
+    
+    args = parser.parse_args()
+    
+    mode = args.user_time_mode
+    maxi = args.max_value    
     
     cond = False #substitute with condition according to the parsed inputs
     
